3cc622a65836a3e0983d6aef2aae98605d72cb64
William Martinez <wdme42@gmail.com>
2023-11-06 01:54:29 -0500

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -6,16 +6,43 @@ import (
 	"os"
 )
 
+const (
+	StatusLine = "HTTP/1.1 200 OK"
+	CRLF       = "\r\n"
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Failed to read from socket:", err)
+		return
+	}
+
+	_, err = conn.Write([]byte(StatusLine + CRLF + CRLF))
+	if err != nil {
+		fmt.Println("Failed to write to socket:", err)
+		return
+	}
+	fmt.Printf("Received %d bytes: %s\n", n, buf[:n])
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Failed to accept connection")
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
 }
